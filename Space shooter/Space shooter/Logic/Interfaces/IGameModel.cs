bcdb0e3c48fc8f1963a4e0c84384073abe9c4137
aliakos13@stud.uni-obuda.hu <aliakos13@stud.uni-obuda.hu>
2022-10-29 14:07:59 +0200

    Logic interfaces inmplemented settings and the basic logic

---
+++ Space shooter/Space shooter/Logic/Interfaces/IGameModel.cs
@@ -0,0 +1,28 @@
+﻿using Space_shooter.Models;
+using Space_shooter.Models.Powerups;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_shooter.Logic.Interfaces
+{
+    public interface IGameModel
+    {
+
+        event EventHandler Changed;
+
+        Player Player { get; set; }
+        Boss Boss { get; set; }
+        List<Laser> Lasers { get; set; }
+        List<Asteroid> Asteroids { get; set; }
+        List<EnemyShip> EnemyShips { get; set; }
+        List<Powerup> Powerups { get; set; }
+
+        int Health { get; }
+        int Score { get; }
+        int HighScore { get; }
+
+    }
+}
---
+++ Space shooter/Space shooter/Logic/Interfaces/ISettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_shooter.Logic.Interfaces
+{
+    public interface ISettings
+    {
+        public enum Difficulty
+        {
+            Easy, Medium, Hard, Custom
+        }
+
+        int Asteroidspeed { get; set; }
+        int Firerate { get; set; }
+        int Poweruprate { get; set; }
+        int Enemyshottimechange { get; set; }
+        int Bossshottimechange { get; set; }
+        bool Godmode { get; set; }
+        bool Sound { get; set; }
+        string PlayerName { get; set; }
+        int EnemySpawnCount { get; set; }
+        int BossHealth { get; set; }
+
+
+        Difficulty Difficultyness { get; set; }
+    }
+}
